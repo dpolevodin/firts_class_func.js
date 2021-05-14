a55036c1908e6062393d4e26887d9f21bfd603fd
Dima <77278863+dpolevodin@users.noreply.github.com>
2021-05-14 14:14:30 +0500

    Update fc_func.js

--- fc_func.js
+++ fc_func.js
@@ -1,7 +1,7 @@
-let passengers = [  { name: 'Jane', paid: true}, 
-                    { name: 'Doctor', paid: true}, 
-                    { name: 'Sue', paid: false}, 
-                    { name: 'John', paid: true} ];
+let passengers = [  { name: 'Jane', paid: true, ticket: 'coach'}, 
+                    { name: 'Doctor', paid: true, ticket: 'firstclass'}, 
+                    { name: 'Sue', paid: false, ticket: 'firstclass'}, 
+                    { name: 'John', paid: true, ticket: 'coach'} ];
 
 let noflylist = ['Doctor'];
 
@@ -30,4 +30,26 @@ if (!allCanFly) {
 let allPaid = processPassenger(passengers, checkNotPaid);
 if (!allPaid) {
     console.log('Мы не можем лететь, есть пассажиры, которые не заплатили');
+}
+
+function printPassenger(passenger) {
+    let passenger_is_paid = passenger.paid;
+    if (passenger_is_paid === true) {
+        console.log(passenger.name + ' оплатил билет');
+    } else {
+        console.log(passenger.name + ' не оплатил билет');
+    }
+}
+
+function createDrinkOrder(passenger) {
+    if (passenger.ticket === 'firstclass') {
+        alert('Would you like cockatail or wine?')
+    } else {
+        alert('Your choice is cola or water?')
+    }
+
+}
+
+function serveCustomer(passenger) {
+    createDrinkOrder();
 }
